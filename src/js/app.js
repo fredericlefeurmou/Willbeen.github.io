160b3436bbe695d14ce5c29b2fe0d27a299a40a5
Frederic <frederic.lefeurmou@gmail.com>
2014-07-13 19:01:18 +0200

    add filters such as langage, metrics, etc.

--- src/js/app.js
+++ src/js/app.js
@@ -3,17 +3,62 @@
 
   app.controller("placeController", ["$http", function($http) {
     var place = this;
-    this.units = "celcius";
-    forecastDays = 7;
+    this.unit = "metric";
+    this.forecastDays = "7";
+    this.lang = "en";
     this.forecast = [];
 
-    this.submitForecast = function(placeName) {
-      $http.get("http://api.openweathermap.org/data/2.5/forecast/daily", {params: {q: placeName, units: place.units, cnt: place.forecastDays}})
-        .success(function(data) {
-          console.log(data);
-          place.name = data.city.name;
-          place.forecast = data.list;
-        });
+    this.submitForecast = function(query) {
+      if (typeof query.name !== "undefined" && query.name !== "") {
+          var queryNameSearch = query.name;
+          if (typeof query.countryCode !== "undefined" && query.countryCode !== "") {
+            queryNameSearch += ',' + place.countryCode;
+          }
+          $http.get("http://api.openweathermap.org/data/2.5/weather", {
+            params: {
+              q: queryNameSearch,
+              units: place.unit,
+              lang: place.lang
+            }})
+            .success(function(data) {
+              place.nameResults = data.name;
+              place.unitResults = place.unit;
+              place.currentWeather = data;
+            });
+          $http.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+            params: {
+              q: queryNameSearch,
+              units: place.unit,
+              cnt: place.forecastDays,
+              lang: place.lang
+            }})
+            .success(function(data) {
+              place.forecast = data.list;
+            });
+      } else if (typeof query.lat !== "undefined" && typeof query.lon !== "undefined") {
+        $http.get("http://api.openweathermap.org/data/2.5/weather", {
+          params: {
+            lat: query.lat,
+            lon: query.lon,
+            units: place.unit,
+            lang: place.lang
+          }})
+          .success(function(data) {
+            place.nameResults = data.name;
+            place.unitResults = place.unit;
+            place.currentWeather = data;
+          });
+        $http.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+          params: {
+            lat: query.lat,
+            lon: query.lon,
+            units: place.unit,
+            cnt: place.forecastDays
+          }})
+          .success(function(data) {
+            place.forecast = data.list;
+          });
+      }
     };
   }]);
 })();
